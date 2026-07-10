feccd932fee79ddedad32170bc2d0b6cd2a9921f
AEI-Mawkib <aeimawkib1@outlook.com>
2026-07-10 18:26:30 -0500

    Delete mawkib-clinic-v4 directory

--- mawkib-clinic-v4/get-logo.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-# One-time (needs internet): downloads the AEI Telehealth logo into public/logo.png
-cd "$(dirname "$0")"
-curl -L -o public/logo.png "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=768,fit=crop/mjEqb1pDVyivLVZW/aei-telehealth-clinic_horizontal-dJobBeypvpse9p19.png" \
-  && echo "Logo saved to public/logo.png" || echo "Download failed - save the logo from aeitelehealth.org as public/logo.png manually"
