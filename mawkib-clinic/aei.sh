9442ffeb771d4410d83dfbce8b53534f66a209f8
AEI-Mawkib <aeimawkib1@outlook.com>
2026-07-20 09:07:29 -0500

    Fix directory path in ae.sh script

--- mawkib-clinic/aei.sh
+++ mawkib-clinic/aei.sh
@@ -3,5 +3,5 @@
 # An effort of Azadar-e-Imam Community to server Zawwars of Imam Hussain a.s.
 # This codebase is open source and freely available to any Mawkib to adapt for their needs
 # For any assistance, you can reach out to us at info@aeitelehealth.org
-cd ~/clinic-2026/mawkib-clinic
+cd ~/clinics-2026/mawkib-clinic
 ./start.sh
